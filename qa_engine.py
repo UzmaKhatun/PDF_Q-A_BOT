61ad9ba3c8de27b6f069c699e5d4eda3002b38e9
Uzma Khatun <uzmakhatun9303@gmail.com>
2025-11-16 15:21:51 +0530

    Update PromptTemplate import to new module
    Updated import statement for PromptTemplate to use the latest version.

--- qa_engine.py
+++ qa_engine.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from sentence_transformers import SentenceTransformer
-from langchain.prompts import PromptTemplate
+# from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate   # updated version for prompt template
 from langchain.schema import Document
 from langchain_groq import ChatGroq
 import numpy as np
